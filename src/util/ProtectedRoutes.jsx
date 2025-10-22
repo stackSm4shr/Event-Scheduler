7ec4130d9a1ed36fd1e6cac2f1ec0d9bb785b094
Tim <timbecker1988@gmail.com>
2025-10-22 11:40:04 +0200

    added error message for user not logged in

--- src/util/ProtectedRoutes.jsx
+++ src/util/ProtectedRoutes.jsx
@@ -2,7 +2,13 @@ import { Outlet, Navigate } from "react-router";
 
 const ProtectedRoutes = () => {
   const user = null; // Simulate an unauthenticated user
-  return user ? <Outlet /> : <Navigate to="/signup" />; // Redirect to login if not authenticated
+
+  if (!user) {
+    alert("You must be logged in to access this page!");
+    return <Navigate to="/signup" />;
+  }
+
+  return <Outlet />;
 };
 
 export default ProtectedRoutes;
