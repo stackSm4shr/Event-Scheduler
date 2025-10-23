e6f05f52dd2afa97f81dd1ed0247cd742fd24a7e
Mohamed Sidali <mohamed.sidali@ymail.com>
2025-10-24 01:48:08 +0200

    Update index.js
    fetch funcs

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,10 @@
 // fetch functions go here
+
+
+export async function fetchEvents() {
+  const response = await fetch('http://localhost:3001/api/events');
+  if (!response.ok) {
+    throw new Error('Failed to fetch events');
+  }
+  return response.json();
+}
