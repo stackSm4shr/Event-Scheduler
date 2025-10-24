34b6ff48d158590fb6a5a10ee186bb46a530cbca
Mohamed Sidali <mohamed.sidali@ymail.com>
2025-10-24 10:50:03 +0200

    updated fetches
    updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,7 @@
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-// fetch functions go here
-=======
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
-
 export async function fetchEvents() {
-  const response = await fetch('http://localhost:3001/api/events');
+  const response = await fetch("http://localhost:3001/api/events");
   if (!response.ok) {
-    throw new Error('Failed to fetch events');
+    throw new Error("Failed to fetch events");
   }
   return response.json();
 }
@@ -20,16 +9,8 @@ export async function fetchEvents() {
 export async function fetchEvent(id) {
   const response = await fetch(`http://localhost:3001/api/events/${id}`);
   if (!response.ok) {
-    throw new Error('Failed to fetch event');
+    throw new Error("Failed to fetch event");
   }
+
   return response.json();
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 }
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
-=======
-}
->>>>>>> Stashed changes
