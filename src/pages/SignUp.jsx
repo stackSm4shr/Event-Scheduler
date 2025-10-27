4f034a31ee415cb4514dff508e69b3fcaee3fde9
Tim <timbecker1988@gmail.com>
2025-10-27 12:36:40 +0100

    create user works, and some cleanup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,22 +1,9 @@
 import SignUpForm from "../components/SignUpForm";
-import { useState } from "react";
 
 export function SignUp() {
-  const [signUpInfo, setsignUpInfo] = useState({
-    firstName: "",
-    email: "",
-    password: "",
-    confirmPassword: "",
-    id: "",
-    token: ""
-
-  });
   return (
     <div>
-      <SignUpForm 
-      signUpInfo = {signUpInfo}
-      setsignUpInfo = {setsignUpInfo}
-      />
+      <SignUpForm />
     </div>
   );
 }
